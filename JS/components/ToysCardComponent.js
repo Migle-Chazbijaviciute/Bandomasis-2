6b810a8ca154ad66e02d75ff04cb9352a83e85af
Miglė Chazbijavičiūtė <miglutech@gmail.com>
2021-11-08 21:58:53 +0200

    fixed [6.] depending on discount type

--- JS/components/ToysCardComponent.js
+++ JS/components/ToysCardComponent.js
@@ -6,7 +6,16 @@ class ToysCardComponent {
   }
 
   createCard = () => {
-    let { id, title, price, ageRestriction, discount, imgSrc } = this.props.data;
+    let { id, title, price, ageRestrictions, discount, imgSrc } = this.props.data;
+    const { currency, amount } = price;
+    if (discount.type === "percentage") {
+      discount = `${discount.amount}%`
+    } else if (discount.type === "amount") {
+      discount = `Minus ${discount.amount}${currency} from the cart total!`
+    } else {
+      discount = `Minus ${discount.amount}${currency} from every unit!`
+    };
+    // console.log(ageRestrictions);
     const element = document.createElement('div');
     element.innerHTML = `
     <div class="border shadow">
@@ -15,15 +24,28 @@ class ToysCardComponent {
     <div class="card-body">
       <h3 class="card-title">${title}</h3>
     </div>
-    <img src="${imgSrc}" class="card-img-top">
+    <img src="${imgSrc}" class="card-img-top pb-3">
     <ul class="list-group">
-      <li class="list-group-item text-muted">Price: ${price.amount} ${price.currency}</li>
-      <li class="list-group-item">Discount: ${discount.amount} ${discount.type}</li>
+      <li class="list-group-item"><b>Price:</b> ${amount} ${currency}</li>
+      <li class="list-group-item"><b>Discount:</b> ${discount}</li>
     </ul>
 </div>
-  </div>
-  
+  </div>`
+
+    const newItem = document.createElement('li');
+    newItem.className = "list-group-item";
+    if (ageRestrictions != null) {
+      newItem.innerHTML = `
+    <b>Age restriction:</b> ${ageRestrictions.from}+
     `
+    } else {
+      newItem.className = "list-group-item text-muted";
+      newItem.innerHTML = `No age restriction.`
+    }
+
+    const ulElement = element.querySelector('.list-group');
+    ulElement.appendChild(newItem);
+
     const deleteBtn = element.querySelector('.delete');
     // debugger;
     deleteBtn.addEventListener('click', () => this.props.onDelete(id));
